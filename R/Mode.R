caefdd36e45047ebd2fbbf04ef46cddda1a25737
Phil Hunter <71961012+p-hunter@users.noreply.github.com>
2021-09-30 17:42:00 +0100

    Mode func changed just for univariate PDFs

--- R/Mode.R
+++ R/Mode.R
@@ -1,21 +1,27 @@
-Mode <- function(x, expected = 1) {
+Mode <- function(x {
   
+
+  # This is only for univariate modes
   # This is not fit for general use - not my fault if things go wrong!
   lx <- length(x)
   
   if(lx >= 20) {
-  pdf_x <- density(x, from = 1)
+  pdf_x <- density(x, from = 0)
   
   pdf_x_m <- matrix(data = c(x = pdf_x$x, y = pdf_x$y), ncol = 2)
   
-  max_y <- sort(pdf_x_m[,2], decreasing = T)[1:expected]
+  max_y <- sort(pdf_x_m[,2], decreasing = T)[1]
   
-  return( pdf_x_m[pdf_x_m[,2] == max_y, 1][1:expected])
+  return( pdf_x_m[pdf_x_m[,2] == max_y, 1][1])
   
   } else if(lx > 5) {
+
     return(median(x, na.rm = TRUE))
+
   } else {
+
     return(mean(x, na.rm = TRUE))
+    
   }
   
   
