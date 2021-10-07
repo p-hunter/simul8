35a8d4b153166ab0d56ee313f9ac2784f21f6ea3
Phil Hunter <71961012+p-hunter@users.noreply.github.com>
2021-10-07 12:36:59 +0100

    attempt at estimating k parameter

--- R/fit_erlang.R
+++ R/fit_erlang.R
@@ -4,22 +4,17 @@ fit_erlang <- function(x, shape  = 2) {
     # currently just returns the scale / rate parameter
     mu <- Mode(x) # a mode of a gaussian kernel density function = scale value of erlang
 
+    if(is.numeric(shape)) {
+        s <- shape
+    } else {
+        s <- as.integer(pmax(1, mean(x, na.rm = TRUE) / mu))
+    }
     
     list(
-        shape = 2,
+        shape = s,
         rate = 1/mu,
         scale = mu
         )
     }
-
-
-
-
-
-
-
-
-
-
 
 }
